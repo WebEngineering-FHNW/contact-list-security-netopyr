41a2003f3f0754577c3e2ae46085bc5d27f24b6a
Michael Heinrichs <netopyr@users.noreply.github.com>
2020-11-02 10:41:50 +0100

    Added mock user service Fixed e2e-test

--- src/main/java/ch/fhnw/webec/contactlistsecurity/config/WebSecurityConfig.java
+++ src/main/java/ch/fhnw/webec/contactlistsecurity/config/WebSecurityConfig.java
@@ -1,10 +1,14 @@
 package ch.fhnw.webec.contactlistsecurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -17,5 +21,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf().disable()
                 .formLogin(Customizer.withDefaults());
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        final User.UserBuilder builder = User.withDefaultPasswordEncoder();
+        final UserDetails user = builder.username("user").password("user").roles("USER").build();
+        final UserDetails admin = builder.username("admin").password("admin").roles("ADMIN", "USER").build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
